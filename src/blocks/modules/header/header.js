3a5cae998d7b8f4f9e8c7d794846a5abf6a03cff
Ruslan <ru@skylinehealthgroup.com>
2023-06-01 17:38:31 +0300

    add humburger menu and media

---
+++ src/blocks/modules/header/header.js
@@ -0,0 +1,16 @@
+"use strict"
+
+const openMobileMenuBtn = document.querySelector(".device-menu");
+const headerMenu = document.querySelector(".header-nav");
+
+openMobileMenuBtn.addEventListener("click", () => {
+    if (openMobileMenuBtn.classList.contains("open")) {
+        openMobileMenuBtn.classList.remove("open");
+        headerMenu.classList.remove("active");
+    } else {
+        headerMenu.classList.add("active");
+        openMobileMenuBtn.classList.add("open");
+    }
+})
+
+
